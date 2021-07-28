6c85d3b41178681a9ed123509f9c38f98266ce16
Alexsandro Xavier <alexsandro060299@outlook.com>
2021-07-28 19:28:23 -0300

    retorna se um dos 3 numeros e par

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio-8.js
@@ -0,0 +1,9 @@
+let numberOne = 30;
+let numberTwo = 77;
+let numberThree = 37;
+
+if(numberOne % 2 == 0 || numberTwo % 2 == 0 ||numberThree % 2 == 0 ){
+  console.log(true);
+} else{
+  console.log(false);
+}
