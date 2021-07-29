6779a3bbf19de2a06ed92b9919c146f5bb6c08f1
Alexsandro Xavier <alexsandro060299@outlook.com>
2021-07-29 20:20:32 -0300

    ex 6

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/ex-6.js
@@ -0,0 +1,18 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+let qtyOdd = 0;
+let message = "";
+for (let i = 0; i < numbers.length; i+=1){
+  if(numbers[i] % 2 != 0){
+    qtyOdd +=1;
+  }else{
+    
+    }
+}
+
+if(qtyOdd !=0){
+  console.log(qtyOdd)
+}else {
+  console.log("nenhum valor ímpar encontrado")
+}
+
