b652279b9aa6ab6dd0c48223b68c433e7bb84182
Alexsandro Xavier <alexsandro060299@outlook.com>
2021-07-29 19:37:18 -0300

    ex 4

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/ex-4.js
@@ -0,0 +1,13 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+let average = 0;
+for (let i = 0; i < numbers.length; i+=1){
+  sum = numbers[i] + sum;
+  average = sum / numbers.length;
+}
+if(average > 20){
+  console.log("Valor maior que 20")
+}else{
+  console.log("Valor menor ou igual a 20")
+}
+
