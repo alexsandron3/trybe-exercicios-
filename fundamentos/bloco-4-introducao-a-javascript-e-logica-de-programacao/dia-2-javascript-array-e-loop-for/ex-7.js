e55da548d8f6d4c0e43448b2d020e06941baab6a
Alexsandro Xavier <alexsandro060299@outlook.com>
2021-07-30 14:44:24 -0300

    Dia 2

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/ex-7.js
@@ -0,0 +1,12 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let lowerNumber;
+for (let i = 0; i < numbers.length; i+=1){
+  if(numbers[i] < numbers[i-1]){
+    if(lowerNumber < ){
+
+    }
+    lowerNumber = numbers[i];
+  }
+  console.log(numbers[i-1])
+}
+console.log(lowerNumber)
