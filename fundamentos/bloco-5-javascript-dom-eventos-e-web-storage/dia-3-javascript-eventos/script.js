fd6260e59ad8352a580e8182c2942785cf482a6e
Alexsandro Xavier <alexsandro060299@outlook.com>
2021-08-11 23:26:22 -0300

    Terminado o exercicio 1

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -1,7 +1,9 @@
 function createDaysOfTheWeek() {
   const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+  
+  
   const weekDaysList = document.querySelector('.week-days');
-
+  
   for (let index = 0; index < weekDays.length; index += 1) {
     const days = weekDays[index];
     const dayListItem = document.createElement('li');
@@ -11,6 +13,22 @@ function createDaysOfTheWeek() {
   };
 };
 
-createDaysOfTheWeek();
+// # EX1
+function createNewElement(element, arrayOfContent, location) {
+
+  for (let index = 0; index < arrayOfContent.length; index += 1) {
+    const content = arrayOfContent[index];
+    const newElement = document.createElement(element);
+    newElement.innerHTML = content;
+    location.appendChild(newElement);
+  }
+
+}
+const dezDays = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+ 
+const days = document.getElementById('days');
+createNewElement('li', dezDays, days);
+
+// createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
