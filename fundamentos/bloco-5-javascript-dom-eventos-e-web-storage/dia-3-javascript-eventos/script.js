1c491199d9d3d129029141e9adb92da9cec02156
Alexsandro Xavier <alexsandro060299@outlook.com>
2021-08-12 00:58:04 -0300

    Conclusao do exercicio 1

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -24,11 +24,36 @@ function createNewElement(element, arrayOfContent, location) {
   }
 
 }
-const dezDays = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
- 
-const days = document.getElementById('days');
-createNewElement('li', dezDays, days);
 
-// createDaysOfTheWeek();
+function newSelector (array, element, elementName){
+  for (let index = 0; index < array.length; index += 1) {
+    switch (elementName) {
+      case 'holiday':
+        element[array[index]].classList.add("holiday");
+        break;
+
+      case 'friday':
+        element[array[index]].classList.add("friday");
+        break;
+
+      default:
+        return 0;
+        break;
+    } 
+  }
+}
+
+// -----------------------------------------------------------------------
+
+const dezDays = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+const idDay = document.getElementById('days');
+const li = document.getElementById('days').children;
+let holidays = [25, 26, 32];
+let fridays = [5, 12, 19, 26]
+createNewElement('li', dezDays, idDay, 'class', 'day');
+newSelector(holidays, li, 'holiday');
+newSelector(fridays, li, 'friday');
+
+createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
